b757a0b76b531692ac5ea1eb15f6fa48c36eea38
AnnaMuhina555 <muhina150411@gmail.com>
2023-09-25 19:44:24 +0100

    making the background

--- main.py
+++ main.py
@@ -4,3 +4,19 @@ WIDTH = 1500
 HEIGHT = 750
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+grass = pygame.image.load("img/grass.jpg")
+grass = pygame.transform.scale(grass, (WIDTH, HEIGHT))
+track = pygame.image.load("img/track.png")
+track = pygame.transform.scale(track, (WIDTH - 400, HEIGHT))
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+    pygame.display.update()
+    screen.blit(grass, (0, 0))
+    screen.blit(track, (200, 0))
+
