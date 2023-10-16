8bdae0b5d2350d912b9f974c498fe3e348ca32fa
AnnaMuhina555 <muhina150411@gmail.com>
2023-10-16 19:15:57 +0100

    nitro0.1

---
+++ nitro.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+class Nitro:
+    def __init__(self, speedUp):
+        self.speedUp = speedUp
+        self.x = 0
+        self.y = 0
+        self.image = pygame.image.load("img/nitro.png")
+        self.show = False
+    
